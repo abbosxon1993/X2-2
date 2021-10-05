66f2a6512a4cf5f6e5b8076963116213db208a6e
Abbosxon Yuldashev <90797368+abbosxon1993@users.noreply.github.com>
2021-10-05 10:24:41 +0500

    Add files via upload

---
+++ ddo.py
@@ -0,0 +1,57 @@
+# class Oquvchi:
+#     def __init__(self, ism, sharf, yosh, vazn, guruh):
+#         self.ism = ism
+#         self.sharf = sharf
+#         self.yosh = yosh
+#         self.vazn = vazn
+#         self.gurug = guruh
+#     def chiqar(self):
+#         print(f"Ismi: {self.ism} Sharfi: {self.sharf} Yoshi: {self.yosh} Vazni: {self.vazn} Guruh: {self.gurug}")
+#
+# hasan = Oquvchi("Hasan", "Sa'dullaev", 24, 70, 15)
+#
+# hasan.chiqar()
+
+# class Mashina:
+#     def __init__(self, turi, nomi, rangi, yili, narxi):
+#         self.type = turi
+#
+#         self.name = nomi
+
+# class dasturchi:
+#     def __init__(self, ism, tillar, maosh):
+#         self.ism = ism
+#         self.tillar = tillar
+#         self.maosh = maosh
+#     def printf(self):
+#         print(f"""
+#     Ism: {self.ism}
+#     Tillar: {self.tillar}
+#     Maosh: {self.maosh}
+#         """)
+#     def ozgartir(self, ishhaqqi, til):
+#         self.maosh += ishhaqqi
+#         self.tillar.append(til)
+#
+# hasan = dasturchi("Hasan", ["C", "Python"], 500)
+# hasan.printf()
+# hasan.ozgartir(200, "Node.Js")
+# hasan.printf()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
